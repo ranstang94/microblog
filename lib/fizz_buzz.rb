3814f04719537c08752d8fd510054e4e0305174a
ranstang94 <randall@branham.org>
2014-01-24 09:20:14 -0500

    Refactored fizz buzz

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,7 +1,7 @@
 class FizzBuzz
 	def self.at(n)
 		case
-		when n % 3 == 0 && n % 5 == 0 then "FizzBuzz"
+		when n % 15 == 0 then "FizzBuzz"
 		when n % 3 == 0 then "Fizz" 
 		when n % 5 == 0 then "Buzz"  
 		else n
